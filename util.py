a769d0627b9ca3a53658e96df6b102f088dc4de3
a1da4 <ai.the.taichi@gmail.com>
2020-05-12 23:53:14 +0900

    delete preprocess module. corpus is loaded in creating co-occur matrix.

--- util.py
+++ util.py
@@ -6,21 +6,10 @@ import _pickle
 from tqdm import tqdm
 
 def load_pickle(pickle_id2word):
-    """ fix text -> id
-
-=======
-import _pickle
-from tqdm import tqdm
-
-def preprocess(corpus, pickle_id2word):
-    """ fix text -> id
-
-    :param texts: sentences
->>>>>>> 432c0265212bba0af3fb12a9412a45db4e4321ad
+    """ load dictionary
     :param id_to_word: dictionary(id->word)
     :param word_to_word: dictionary(word->id)
     
-<<<<<<< HEAD
     :return: id2word, word2id
     """ 
     word_to_id = {}
@@ -33,52 +22,7 @@ def preprocess(corpus, pickle_id2word):
 
 
 def create_co_matrix(file_path, word_to_id, vocab_size, window_size):
-=======
-    :return: corpus_replaced(fixed word into id), word2id, id2word
-    """ 
-    make_new_dic = False
-    id_to_word = {}
-    word_to_id = {}
-
-    try:
-        fp = open(pickle_id2word, 'rb')
-        id_to_word = _pickle.load(fp)
-        for id in id_to_word:
-            word_to_id[id_to_word[id]] = id
-
-    except:
-        make_new_dic = True
-
-    corpus_replaced = []
-    
-    # out of vocab
-    id_to_word[-1] = '#'
-    word_to_id['#'] = -1
-
-    for text in tqdm(corpus):
-        words = text.split(" ")
-        words = [w for w in words if len(w) > 0]
-        for word in words:
-            if word not in id_to_word.values():
-                if make_new_dic:
-                    new_id = len(word_to_id)-1
-                    id_to_word[new_id] = word
-                    word_to_id[word] = new_id
-                else:
-                    words[words.index(word)] = '#'
-        corpus_replaced.append(np.array([word_to_id[w] for w in words]))
-    
-    if make_new_dic:
-        fp = open('dic_id2word.pkl', 'wb')
-        _pickle.dump(id_to_word, fp)
-
-    return corpus_replaced, id_to_word
-
-
-def create_co_matrix(corpus, vocab_size, window_size):
->>>>>>> 432c0265212bba0af3fb12a9412a45db4e4321ad
-    """create cooccur matrix
-
+    """ create co-occur matrix
     :param corpus: corpus(fixed into id)
     :param vocab_size: vocab size
     :param window_size: windows size for counting
@@ -87,33 +31,12 @@ def create_co_matrix(corpus, vocab_size, window_size):
     """
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 
-<<<<<<< HEAD
     import time
     start = time.time()
 
     with open(file_path) as fp:
         for sentence in fp:
             words = re.sub(r'\n', '', sentence).split(' ')
-=======
-    for sentence in tqdm(corpus):
-        for idx, word_id in enumerate(sentence):
-            if word_id == -1:
-                continue
-            for i in range(1, window_size + 1):
-                left_idx = idx - i
-                right_idx = idx + i
-
-                if left_idx >= 0:
-                    left_word_id = sentence[left_idx]
-                    if left_word_id != -1:
-                        co_matrix[word_id, left_word_id] += 1
-
-                if right_idx < len(sentence):
-                    right_word_id = sentence[right_idx]
-                    if right_word_id != -1:
-                        co_matrix[word_id, right_word_id] += 1
->>>>>>> 432c0265212bba0af3fb12a9412a45db4e4321ad
-
             for idx, word in enumerate(words):
                 if word in word_to_id:
                     word_id = word_to_id[word]
@@ -154,7 +77,6 @@ def threshold_cooccur(C, threshold):
 
 def absolute_discounting(C, i, j, d):
     """ SMOOTHING: absolute discounting
-
     :param C: cooccur matrix
     :param i, j: index
     :param d: discounting value (0, 1)
@@ -164,10 +86,6 @@ def absolute_discounting(C, i, j, d):
 
     :return: smoothed value
     """
-
-    #discounting = max(C[i][j] - d, 0)
-    #smoothing = d * np.count_nonzero(C[i]) / C.shape[1]
-    #return discounting + smoothing
 
     if C[i][j] > 0:
         return C[i][j] - d
@@ -180,7 +98,6 @@ def absolute_discounting(C, i, j, d):
 
 def sppmi(C, k, eps=1e-8, has_abs_dis=False):
     """ compute Shifted Positive PMI (SPPMI)
-
     :param C: cooccur matrix
     :param k: number of negative samples in w2v sgns
     :param has_abs_dis: bool, do absolute discounting smoothing or not
